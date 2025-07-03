7d337a9b1359f8c4e4e0a3cb2a796a91d23c51e6
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-07-03 17:42:27 +0700

    Update storybook and unit test for pagination component

--- shadcn-training/src/components/pagination/pagination.test.tsx
+++ shadcn-training/src/components/pagination/pagination.test.tsx
@@ -7,7 +7,10 @@ import { Pagination } from '.';
 
 describe('Pagination component', () => {
   test('Renders pagination component', () => {
-    const { container } = render(<Pagination />);
+    const handlePageChange = jest.fn();
+    const { container } = render(
+      <Pagination totalPages={5} currentPage={1} onPageChange={handlePageChange} />,
+    );
     expect(container).toMatchSnapshot();
   });
 });
