4719d6fc0003f61be4e4f4810939a1f7d9562777
Hoa Nguyen <hoa.nguyen@asnet.com.vn>
2025-07-21 15:31:32 +0700

    Improve coverage score for component tab

--- shadcn-training/src/components/common/tabs/__test__/tabs.test.tsx
+++ shadcn-training/src/components/common/tabs/__test__/tabs.test.tsx
@@ -1,6 +1,6 @@
 // Libs
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 
 // Components
 import { Tabs } from '..';
@@ -12,5 +12,18 @@ describe('Tabs component', () => {
   test('Renders tabs component', () => {
     const { container } = render(<Tabs tabs={TABS_LABEL} cards={PRODUCTS_HOME_PAGE} />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('View More button increases visible cards', () => {
+    render(<Tabs tabs={TABS_LABEL} cards={PRODUCTS_HOME_PAGE} />);
+
+    const initialCards = screen.getAllByTestId('product-card');
+    expect(initialCards.length).toBeLessThanOrEqual(6);
+
+    const viewMoreButton = screen.getByRole('button', { name: /view more/i });
+    fireEvent.click(viewMoreButton);
+
+    const updatedCards = screen.getAllByTestId('product-card');
+    expect(updatedCards.length).toBeGreaterThan(initialCards.length);
   });
 });
